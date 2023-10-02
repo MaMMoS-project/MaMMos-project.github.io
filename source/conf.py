a37a09ff6a9556869248deb577b44c1c1a44630c
Martin Lang <m.lang@soton.ac.uk>
2023-10-02 10:16:27 +0200

    Minimal website

---
+++ source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MaMMoS'
+copyright = '2023, MaMMos collaboration'
+author = 'MaMMos collaboration'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_title = 'MaMMoS'  # instead of the logo (if not set, defaults to 'MaMMoS documentation')
+html_static_path = ['_static']
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/mammos-project",
+            "icon": "fab fa-github-square",
+        },
+    ],
+    "show_prev_next": False,
+}
---
+++ source/index.rst
@@ -0,0 +1,14 @@
+.. MaMMoS documentation master file, created by
+   sphinx-quickstart on Mon Oct  2 09:55:58 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+**Ma**\gnetic **M**\ultiscale **Mo**\delling **S**\uite
+=======================================================
+
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   open-positions
+
