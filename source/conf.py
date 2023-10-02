78cf4d85b6c1b3c9379d88112e9dd4af5e2422cc
Martin Lang <m.lang@soton.ac.uk>
2023-10-02 10:33:54 +0200

    Prepare automatic deployment

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,9 @@ author = 'MaMMos collaboration'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.githubpages',  # creates .nojekyll file in HTML directory
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
